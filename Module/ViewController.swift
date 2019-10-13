838d62f93d8b012ff02b93075ad2943229466678
NealWills <nealwills93@gmail.com>
2019-10-14 00:01:49 +0800

    Initial Commit
    Initial Commit

--- Module/ViewController.swift
+++ Module/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
+import ECGRect
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let view = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 200))
+        
+        print(view.nw.leading)
+        
+        print(view.nw.width)
+        
+        print(view.nw.height)
+        
     }
 
 
